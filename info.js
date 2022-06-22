69e8b2f44e7f4df6d3d3f12805b34261e944aece
ibukub <ibukunatolagbe@gmail.com>
2022-06-22 22:52:15 +0100

    first commit

---
+++ info.js
@@ -0,0 +1,8 @@
+let firstName = "Ibukun"
+let secondName = "Atolage"
+let height = 5.3;
+let country = 'Nigeria';
+alert('First Name: ' + firstName);
+alert('Second Name: ' + secondName);
+alert('Height: ' + height);
+alert('Country: ' + country);
